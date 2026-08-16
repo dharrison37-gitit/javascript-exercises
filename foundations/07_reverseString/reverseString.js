54ad6d9ef926bdf56f60312c85c72ae6d580428c
dharrison37-gitit <308913916+dharrison37-gitit@users.noreply.github.com>
2026-08-16 01:38:47 -0500

    Complete exercise 07_reverseString
    Completed function to reverse a given string and
    return it.
    
    All tests passing.

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
+const reverseString = function(str) {
+    let splitString = str.split("");
+    let reversed = [];
 
+    for (let i = str.length - 1; i >= 0; i--) {
+        reversed.push(str[i]);
+    }
+
+    return reversed.join("");
 };
 
 // Do not edit below this line

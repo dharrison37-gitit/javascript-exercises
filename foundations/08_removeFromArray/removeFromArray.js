49cf5731f5d48bfd973d0b1134ca280aa647e1a7
dharrison37-gitit <308913916+dharrison37-gitit@users.noreply.github.com>
2026-08-16 16:41:02 -0500

    Complete exercise 08_removeFromArray
    Completed function removeFromArray() to remove passed
    in arguments.
    
    All tests are passing.

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,4 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...rest) {
+    const keep = [];
+    for(item of arr) {
+        if(rest.includes(item)) {
+            continue;
+        }
+        keep.push(item);
+    }
+    return keep;
 };
 
 // Do not edit below this line

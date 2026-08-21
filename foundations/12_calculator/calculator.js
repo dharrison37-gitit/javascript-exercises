26cba93b4147efd87deb578fc4b7a1390f45924a
dharrison37-gitit <308913916+dharrison37-gitit@users.noreply.github.com>
2026-08-20 21:48:42 -0500

    Complete exercise 12_calculator
    Implemented all functions
    
    All tests are passing.

--- foundations/12_calculator/calculator.js
+++ foundations/12_calculator/calculator.js
@@ -1,33 +1,31 @@
-const add = function() {
-	
-};
+const add = (a, b) => a + b;
 
-const subtract = function() {
-	
-};
+const subtract = (a, b) => a - b;
 
-const sum = function() {
-	
-};
+const sum = (arr) => arr.reduce((accum, current) => accum + current, 0);
 
-const multiply = function() {
+const multiply = (arr) => arr.reduce((accum, current) => accum * current, 1);
 
-};
+const power = (base, exponent) => base ** exponent;
 
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
+const factorial = function (n) {
+    if (n === 0) {
+        return 1;
+    } else {
+        let fact = 1;
+        for (let i = 1; i <= n; i++) {
+            fact *= i;
+        }
+        return fact;
+    }
 };
 
 // Do not edit below this line
 module.exports = {
-  add,
-  subtract,
-  sum,
-  multiply,
-  power,
-  factorial
+    add,
+    subtract,
+    sum,
+    multiply,
+    power,
+    factorial,
 };

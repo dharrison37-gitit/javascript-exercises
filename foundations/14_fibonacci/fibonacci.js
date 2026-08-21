5d93fad144ae02ed19e8721e542cec638f8f9f38
dharrison37-gitit <308913916+dharrison37-gitit@users.noreply.github.com>
2026-08-20 23:29:36 -0500

    Complete exercise 14_fibonacci
    Comleted function to return n-th number in the
    Fibonacci sequence.
    
    -- Another confusing exercise - had to peek on this
        one too. Really irritating!

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,5 +1,15 @@
-const fibonacci = function() {
+const fibonacci = function (n) {
+    n = parseInt(n);
 
+    if (n < 0) return "OOPS";
+
+    let fibs = [0, 1];
+
+    for (let i = 2; i <= n; i++) {
+        fibs[i] = fibs[i - 1] + fibs[i - 2];
+    }
+
+    return fibs[n];
 };
 
 // Do not edit below this line

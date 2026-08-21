6af8b387627e258cf560847a31af69ef5567489a
dharrison37-gitit <308913916+dharrison37-gitit@users.noreply.github.com>
2026-08-21 01:59:36 -0500

    Complete exercise 16_findTheOldest
    Implemented the function to find the oldest person
    in a given array of objects of people.
    
    Solution could be better as my function does more
    than one thing!

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,5 +1,14 @@
-const findTheOldest = function() {
-
+const findTheOldest = function (people) {
+    for (person of people) {
+        if (!person.yearOfDeath) {
+            person.yearOfDeath = new Date().getFullYear();
+        }
+    }
+    const sorted = people.sort(
+        (a, b) =>
+            a.yearOfDeath - a.yearOfBirth - (b.yearOfDeath - b.yearOfBirth),
+    );
+    return sorted.pop();
 };
 
 // Do not edit below this line

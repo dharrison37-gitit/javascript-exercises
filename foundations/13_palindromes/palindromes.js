a07d6d995a421c64c120a7a4fae8269391fb20c9
dharrison37-gitit <308913916+dharrison37-gitit@users.noreply.github.com>
2026-08-20 22:37:43 -0500

    Complete exercise 13_palindrome
    Had to peek solution on this one because I got
    stuck and confused. Have to be careful how to do
    TDD.
    
    Understanding of TDD:
    -- run test
    -- test fails
    -- make test pass (problem starter here)
    ---- simply return true and most test pass but are
    ---- not correct!
    -- grow HUGE problem that causes confusion

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,6 +1,18 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    const alpha = "abcdefghijklmonpqrstuvwxyz0123456789";
 
+    let workingString = str
+        .toLowerCase()
+        .split("")
+        .filter((c) => alpha.includes(c))
+        .join("");
+
+    let revStr = workingString.split("").reverse().join("");
+
+    return workingString === revStr;
 };
+
+palindromes("A car, a man, a maraca");
 
 // Do not edit below this line
 module.exports = palindromes;
